20159b672f8fc5d441a4e781f385fd42d32fe5a4
Lucas O. S <ovniroto@gmail.com>
2023-09-18 17:05:31 +0200

    seo: add meta tags

--- src/routes/_app.tsx
+++ src/routes/_app.tsx
@@ -4,11 +4,13 @@ import Stars from "../components/Stars.tsx"
 
 export default function App({ Component }: AppProps) {
 	return (
-		<html>
+		<html lang="en">
 		<head>
 			<meta charSet="utf-8" />
 			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			<title>Super Secure Password Generator - OVNI.pw</title>
+			<meta name="description" content="On this website you can create a super secure password without having to store or remember it."/>
+			<meta name="robots" content="index, follow" />
 			<link rel="stylesheet" href={ asset("/styles/global.css") } />
 			<link href="https://cdn.jsdelivr.net/npm/daisyui@3.6.4/dist/full.css" rel="stylesheet" type="text/css" />
 			<link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico?v=1" />
